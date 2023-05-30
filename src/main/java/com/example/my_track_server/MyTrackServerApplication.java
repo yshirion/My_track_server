979a345aeceac6aeff9a4790b5fcda2edc8589b5
Yechiel Shirion <100494204+yshirion@users.noreply.github.com>
2023-05-30 18:20:53 +0300

    Initial commit

---
+++ src/main/java/com/example/my_track_server/MyTrackServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_track_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTrackServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTrackServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/my_track_server/MyTrackServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_track_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTrackServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
